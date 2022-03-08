f1d0676ffd116a05da21dfad8f4d3e198bf5dfd7
Sadeeqrabiu <sadeeqrabiu373@gmail.com>
2022-03-08 22:33:13 +0100

    fiber added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fiber_crud
+
+
+Hey... are you a golang nerd? 
+if yes welcome comrade. we have work to do hhaha you know what i mean cahnging the world with go 
+
+enjoy the Crud code 
--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type JSONTextResponse struct {
+	Message string
+}
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+
+		return c.JSON(JSONTextResponse{Message: "hello fiber"})
+
+	})
+
+	app.Listen(":8080")
+}
